df14a5aead8e1b06df5efce3603f9034f3652df1
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2024-03-12 00:04:30 -0400

    Created Navigation Action
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- NavDemo/app/src/main/java/com/example/navdemo/HomeFragment.kt
+++ NavDemo/app/src/main/java/com/example/navdemo/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.navdemo.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -20,6 +21,7 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        binding.button.setOnClickListener { it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment) }
         return binding.root
     }
 }
