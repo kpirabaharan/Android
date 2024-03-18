c489aa8e4ff69864a0e0b060a0d90646d3a13817
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2024-03-17 20:52:44 -0400

    RecyclerView Demo Complete
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MainActivity.kt
+++ RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MainActivity.kt
@@ -3,16 +3,35 @@ package com.example.recyclerviewdemo
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    val fruitsList = listOf<Fruit>(
+        Fruit("Mango", "Joe"),
+        Fruit("Apple", "Frank"),
+        Fruit("Banana", "Tom"),
+        Fruit("Orange", "Joe"),
+        Fruit("Pineapple", "Alex"),
+        Fruit("Grapes", "Joe"),
+        Fruit("Watermelon", "Alex"),
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recyvlerView = findViewById<RecyclerView>(R.id.myRecyclerView)
         recyvlerView.setBackgroundColor(Color.YELLOW)
         recyvlerView.layoutManager = LinearLayoutManager(this)
-        recyvlerView.adapter = MyRecyclerViewAdapter()
+        recyvlerView.adapter = MyRecyclerViewAdapter(fruitsList) { selectedItem: Fruit ->
+            listItemClicked(
+                selectedItem
+            )
+        }
+    }
+
+    private fun listItemClicked(fruit:Fruit) {
+        Toast.makeText(this@MainActivity, "Supplier is: ${fruit.supplier}", Toast.LENGTH_SHORT).show()
     }
 }
--- RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MyRecyclerViewAdapter.kt
+++ RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MyRecyclerViewAdapter.kt
@@ -4,9 +4,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class MyRecyclerViewAdapter: RecyclerView.Adapter<MyViewHolder>() {
+class MyRecyclerViewAdapter(private val fruitsList: List<Fruit>, private val clickListener:(Fruit) -> Unit) : RecyclerView.Adapter<MyViewHolder>() {
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val listItem = layoutInflater.inflate(R.layout.list_item, parent, false)
@@ -15,14 +18,22 @@ class MyRecyclerViewAdapter: RecyclerView.Adapter<MyViewHolder>() {
 
     // Display data on ListItem
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-
+        val fruit = fruitsList[position]
+        holder.bind(fruit, clickListener)
     }
 
     override fun getItemCount(): Int {
-        return 5
+        return fruitsList.size
     }
 }
 
-class MyViewHolder(val view: View): RecyclerView.ViewHolder(view){
-    val myTextView = view.findViewById<TextView>(R.id.tvName)
+class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+    fun bind(fruit: Fruit, clickListener: (Fruit) -> Unit) {
+        val myTextView = view.findViewById<TextView>(R.id.tvName)
+        myTextView.text = fruit.name
+
+        view.setOnClickListener {
+            clickListener(fruit)
+        }
+    }
 }
