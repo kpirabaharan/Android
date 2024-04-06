c8c5a4828180e7e3f01c6d04c8488e5ac61a80a7
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2024-04-06 16:55:08 -0400

    Room Library Demo Project Init and Entity Classes
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ RoomDemo/app/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.example.roomdemo"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.roomdemo"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    dataBinding {
+        enable = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    //    LiveData
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    //    ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    //    Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+    // Room
+    implementation(libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+}
